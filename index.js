0b3eedf758afc2f618ef38d710ea2e9e7e12c8d6
C. Scott Ananian <cscott@cscott.net>
2016-03-31 18:17:29 -0400

    Use npm `telnet` package to greatly improve telnet protocol support.
    We also hook up 'readline' so we get tab-completion and history and
    other good stuff.

--- index.js
+++ index.js
@@ -6,8 +6,9 @@
 // npm install mw-ocg-texter
 
 var fs = require('fs');
-var net = require('net');
 var path = require('path');
+var readline = require('readline');
+var telnet = require('telnet');
 
 var texter = require('mw-ocg-texter/lib/standalone');
 
@@ -58,58 +59,109 @@ var logoP = getLogoP();
 // Refresh this every six hours.
 setInterval(function() { logoP = getLogoP(); }, 6*60*60*1000 );
 
-function recv(socket, data) {
-	data = data.toString();
-	if (data.charCodeAt(0) === 255) {
-		// 255 is a telnet control char.
-		data = data.slice(1);
-		switch (data.charCodeAt(0)) {
-			case 255: // byte value 255
-				break;
-			case 246: // Are you there?
-				socket.write('Still around! What do you want to read next?\n');
-				socket.write(ps1);
-				return;
-			case 241: // NOP
-			default:
-				// Ignore the rest
-				socket.write(ps1);
-				return;
-		}
-	}
+function completer(line) {
+	var completions = 'quit|use en.wikipedia.org|use es.wikipedia.org|use ja.wikipedia.org|use de.wikipedia.org|use ru.wikipedia.org|use fr.wikipedia.org|use it.wikipedia.org|use pt.wikipedia.org|use zh.wikipedia.org|use pl.wikipedia.org'.split('|');
+	var hits = completions.filter(function(c) { return c.indexOf(line) == 0; });
+	return [hits.length ? hits : completions, line];
+}
 
-	data = data.replace(/(\r\n?|\n)/gm, '').trim();
+function recv(rl, client, line) {
+	rl.pause();
+	line = line.trim();
 
-	var m = /^(host|use)\s+(\S+\.org)\s*$/i.exec(data);
+	var m = /^(host|use)\s+(\S+\.org)$/i.exec(line);
 	if (m) {
 		domain = m[2];
-		socket.write('Using '+domain+' for future articles.\n');
-		socket.write(ps1);
+		client.write('Using '+domain+' for future articles.\n');
+		rl.prompt();
 		return;
 	}
 
-	if (data === 'quit') {
-		socket.end('Bye!\n');
+	if (line === 'quit') {
+		client.write('Bye!\n');
+		rl.close();
 		return;
 	}
 
 	texter.convert({
 		domain: domain,
-		title: data,
-		stream: socket,
+		title: line,
+		stream: client,
 		// siteinfo cacher is optional, but it speeds things up
 		// by eliminating an unnecessary action API request for each article
 		siteinfo: siteinfoCacher,
 	}).catch(function(e) {
-		socket.write('Sorry! Could not fetch "' + data + '" for you.\nNo worries. There are lots of other pages to read.\nPick a different title.\n');
-	}).then(function() { socket.write(ps1); });
+		client.write('Sorry! Could not fetch "' + line + '" for you.\n' +
+					 'No worries. There are lots of other pages to read.\n' +
+					 'Pick a different title.\n');
+	}).then(function() { rl.prompt(); });
 }
 
-console.log('Listening on port', port);
-net.createServer(function (socket) {
+var server = telnet.createServer(function (client) {
+	client.on('window size', function(e) {
+		if (e.command === 'sb') {
+			// A real "resize" event; readline listens for this.
+			client.columns = e.columns;
+			client.rows = e.rows;
+			client.emit('resize');
+		}
+	});
+	// 'readline' will call `setRawMode` when it is a function
+	client.setRawMode = setRawMode;
+	// Make unicode characters work properly
+	client.do.transmit_binary();
+	// Make the client emit 'window size' events
+	client.do.window_size();
+
+	// Create a read line interface.
+	var rl = readline.createInterface({
+		input: client,
+		output: client,
+		terminal: true,
+		completer: completer
+	});
+	rl.setPrompt(ps1); rl.pause();
+	rl.on('close', function() { client.end(); });
+
 	logoP.then(function(logo) {
-		socket.write(logo);
-		socket.write(ps1);
-		socket.on('data', recv.bind(null, socket));
+		client.write(logo);
+		rl.prompt();
+		rl.on('line', recv.bind(null, rl, client));
 	} );
-}).listen(port);
+});
+
+server.on('error', function (err) {
+	if (err.code === 'EACCES') {
+		console.error(
+			'%s: You must be "root" to bind to port %d', err.code, port
+		);
+	} else {
+		throw err;
+	}
+});
+
+server.on('listening', function () {
+	console.log(
+		'wikipedia telnet server listening on port %d', this.address().port
+	);
+	console.log('  $ telnet localhost' + (port != 23 ? ' ' + port : ''));
+});
+
+server.listen(port);
+
+/**
+ * The equivalent of "raw mode" via telnet option commands.
+ * Set this function on a telnet `client` instance.
+ */
+
+function setRawMode (mode) {
+	if (mode) {
+		this.do.suppress_go_ahead();
+		this.will.suppress_go_ahead();
+		this.will.echo();
+	} else {
+		this.dont.suppress_go_ahead();
+		this.wont.suppress_go_ahead();
+		this.wont.echo();
+	}
+}
